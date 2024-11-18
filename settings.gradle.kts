5e5ab6798925226e50876c8d84071a52ee1ef526
popipopka <eg.belykh@yandex.ru>
2024-11-18 18:54:51 +0300

    fix(usecase): add usecase module in gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,3 +5,9 @@ project(":domain").apply {
     projectDir = file("core/domain")
     buildFileName = "build.gradle.kts"
 }
+
+include(":usecase")
+project(":usecase").apply {
+    projectDir = file("core/usecase")
+    buildFileName = "build.gradle.kts"
+}
