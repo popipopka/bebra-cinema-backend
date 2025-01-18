2c2a8f6619a43831f135e570aef94bf52d27d871
popipopka <eg.belykh@yandex.ru>
2025-01-19 01:57:12 +0300

    feat: add mavenBom to the main build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,13 +17,15 @@ subprojects {
     apply(plugin = "io.spring.dependency-management")
 
     dependencyManagement {
+        imports {
+            mavenBom("org.springframework.boot:spring-boot-dependencies:3.3.3")
+        }
+
         dependencies {
             dependencySet("org.mapstruct:1.5.5.Final") {
                 entry("mapstruct")
                 entry("mapstruct-processor")
             }
-
-            dependency("org.projectlombok:lombok:1.18.36")
 
             dependency("org.projectlombok:lombok-mapstruct-binding:0.2.0")
 
